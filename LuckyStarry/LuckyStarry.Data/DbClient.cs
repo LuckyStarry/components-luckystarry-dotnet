1473d67a5f4b9ccaca5a84e13d0cd149b81e496b
SUN BO <starry@vip.qq.com>
2017-12-15 17:57:01 +0800

    db

---
+++ LuckyStarry/LuckyStarry.Data/DbClient.cs
@@ -0,0 +1,75 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LuckyStarry.Data
+{
+    public class DbClient : IDbClient
+    {
+        private IServiceProvider services { get; }
+        public string DbName { get; }
+
+        public DbClient(IServiceCollection services, string dbName)
+        {
+            this.DbName = dbName;
+        }
+
+        public static T Execute<T>(IDbConnection connection, Func<IDbConnection, T> func)
+        {
+            using (connection)
+            {
+                return func(connection);
+            }
+        }
+
+        public virtual IDbConnection CreateConnection()
+        {
+        }
+        public virtual T Execute<T>(Func<IDbConnection, T> func)
+        {
+            return DbClient.Execute<T>(this.CreateConnection(), func);
+        }
+
+        public virtual IEnumerable<T> Query<T>(string sqlText, object param = null)
+        {
+            return this.Execute(connection => connection.Query<T>(sqlText, param));
+        }
+        public virtual T QueryFirst<T>(string sqlText, object param = null)
+        {
+            return this.Execute(connection => connection.QueryFirst<T>(sqlText, param));
+        }
+        public virtual T QueryFirstOrDefault<T>(string sqlText, object param = null)
+        {
+            return this.Execute(connection => connection.QueryFirstOrDefault<T>(sqlText, param));
+        }
+        public virtual T ExecuteScalar<T>(string sqlText, object param = null)
+        {
+            return this.Execute(connection => connection.ExecuteScalar<T>(sqlText, param));
+        }
+        public virtual int ExecuteNonQuery(string sqlText, object param = null)
+        {
+            return this.Execute(connection => connection.Execute(sqlText, param));
+        }
+
+        public virtual T Transaction<T>(Func<IDbConnection, IDbTransaction, T> func)
+        {
+            return this.Execute(connection =>
+            {
+                var transaction = connection.BeginTransaction();
+                try
+                {
+                    var result = func(connection, transaction);
+                    transaction.Commit();
+                    return result;
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            });
+        }
+    }
+}
---
+++ LuckyStarry/LuckyStarry.Data/IDbClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace LuckyStarry.Data
+{
+    public interface IDbClient
+    {
+        string DbName { get; }
+        IDbConnection CreateConnection();
+        T Execute<T>(Func<IDbConnection, T> func);
+        int ExecuteNonQuery(string sqlText, object param = null);
+        T ExecuteScalar<T>(string sqlText, object param = null);
+        IEnumerable<T> Query<T>(string sqlText, object param = null);
+        T QueryFirst<T>(string sqlText, object param = null);
+        T QueryFirstOrDefault<T>(string sqlText, object param = null);
+
+        T Transaction<T>(Func<IDbConnection, IDbTransaction, T> func);
+    }
+}
