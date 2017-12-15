3d67d27be25c8f8ff5bac8273dc2b970db5d543b
SUN BO <starry@vip.qq.com>
2017-12-15 15:46:09 +0800

    JsonSmart +

---
+++ LuckyStarry/LuckyStarry.Models/IJsonSmart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LuckyStarry.Models
+{
+    public interface IJsonSmart
+    {
+        bool IsSuccessful { get; }
+        string Message { get; }
+    }
+
+    public interface IJsonSmart<T> : IJsonSmart
+    {
+        T Entity { get; }
+    }
+}
---
+++ LuckyStarry/LuckyStarry.Models/JsonSmartExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuckyStarry.Models
+{
+    public static class JsonSmartExtensions
+    {
+        public static T IsSuccessful<T>(this T model) where T : JsonSmart
+        {
+            model.IsSuccessful = true;
+            return model;
+        }
+
+        public static T IsFailed<T>(this T model) where T : JsonSmart
+        {
+            model.IsSuccessful = false;
+            return model;
+        }
+
+        public static T WithMessage<T>(this T model, string message) where T : JsonSmart
+        {
+            model.Message = message;
+            return model;
+        }
+
+        public static T WithEntity<T, U>(this T model, U entity) where T : JsonSmart<U>
+        {
+            model.Entity = entity;
+            return model;
+        }
+    }
+}
