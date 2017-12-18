3f6f06d851597e732a8a010a2f06c16c35971902
SUN BO <starry@vip.qq.com>
2017-12-18 18:19:56 +0800

    IDbClientFactory

---
+++ LuckyStarry/LuckyStarry.Data/IDbClientFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuckyStarry.Data
+{
+    public interface IDbClientFactory
+    {
+        IDbClient Create(string dbName);
+    }
+}
