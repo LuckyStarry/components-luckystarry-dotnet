3726f6381677938d77da5f811ab6315eb50b1d19
SUN BO <starry@vip.qq.com>
2017-12-19 16:28:52 +0800

    DbClient Async Functions +

--- LuckyStarry/LuckyStarry.Data/DbClient.cs
+++ LuckyStarry/LuckyStarry.Data/DbClient.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Threading.Tasks;
 
 namespace LuckyStarry.Data
 {
@@ -24,36 +25,30 @@ namespace LuckyStarry.Data
                 return func(connection);
             }
         }
+        public virtual T Execute<T>(Func<IDbConnection, T> func) => DbClient.Execute<T>(this.CreateConnection(), func);
+        public static async Task<T> ExecuteAsync<T>(IDbConnection connection, Func<IDbConnection, Task<T>> func)
+        {
+            using (connection)
+            {
+                return await func(connection);
+            }
+        }
+        public virtual async Task<T> ExecuteAsync<T>(Func<IDbConnection, Task<T>> func) => await DbClient.ExecuteAsync<T>(await this.CreateConnectionAsync(), func);
 
-        public virtual IDbConnection CreateConnection()
-        {
-            return this.factory.Create(this.DbName);
-        }
-        public virtual T Execute<T>(Func<IDbConnection, T> func)
-        {
-            return DbClient.Execute<T>(this.CreateConnection(), func);
-        }
+        public virtual IDbConnection CreateConnection() => this.factory.Create(this.DbName);
+        public virtual async Task<IDbConnection> CreateConnectionAsync() => await Task.FromResult(this.CreateConnection());
 
-        public virtual IEnumerable<T> Query<T>(string sqlText, object param = null)
-        {
-            return this.Execute(connection => connection.Query<T>(sqlText, param));
-        }
-        public virtual T QueryFirst<T>(string sqlText, object param = null)
-        {
-            return this.Execute(connection => connection.QueryFirst<T>(sqlText, param));
-        }
-        public virtual T QueryFirstOrDefault<T>(string sqlText, object param = null)
-        {
-            return this.Execute(connection => connection.QueryFirstOrDefault<T>(sqlText, param));
-        }
-        public virtual T ExecuteScalar<T>(string sqlText, object param = null)
-        {
-            return this.Execute(connection => connection.ExecuteScalar<T>(sqlText, param));
-        }
-        public virtual int ExecuteNonQuery(string sqlText, object param = null)
-        {
-            return this.Execute(connection => connection.Execute(sqlText, param));
-        }
+        public virtual IEnumerable<T> Query<T>(string sqlText, object param = null) => this.Execute(connection => connection.Query<T>(sqlText, param));
+        public virtual T QueryFirst<T>(string sqlText, object param = null) => this.Execute(connection => connection.QueryFirst<T>(sqlText, param));
+        public virtual T QueryFirstOrDefault<T>(string sqlText, object param = null) => this.Execute(connection => connection.QueryFirstOrDefault<T>(sqlText, param));
+        public virtual T ExecuteScalar<T>(string sqlText, object param = null) => this.Execute(connection => connection.ExecuteScalar<T>(sqlText, param));
+        public virtual int ExecuteNonQuery(string sqlText, object param = null) => this.Execute(connection => connection.Execute(sqlText, param));
+
+        public virtual async Task<IEnumerable<T>> QueryAsync<T>(string sqlText, object param = null) => await this.ExecuteAsync(connection => connection.QueryAsync<T>(sqlText, param));
+        public virtual async Task<T> QueryFirstAsync<T>(string sqlText, object param = null) => await this.ExecuteAsync(connection => connection.QueryFirstAsync<T>(sqlText, param));
+        public virtual async Task<T> QueryFirstOrDefaultAsync<T>(string sqlText, object param = null) => await this.ExecuteAsync(connection => connection.QueryFirstOrDefaultAsync<T>(sqlText, param));
+        public virtual async Task<int> ExecuteNonQueryAsync(string sqlText, object param = null) => await this.ExecuteAsync(connection => connection.ExecuteAsync(sqlText, param));
+        public virtual async Task<T> ExecuteScalarAsync<T>(string sqlText, object param = null) => await this.ExecuteAsync(connection => connection.ExecuteScalarAsync<T>(sqlText, param));
 
         public virtual T Transaction<T>(Func<IDbConnection, IDbTransaction, T> func)
         {
@@ -63,6 +58,24 @@ namespace LuckyStarry.Data
                 try
                 {
                     var result = func(connection, transaction);
+                    transaction.Commit();
+                    return result;
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            });
+        }
+        public virtual async Task<T> TransactionAsync<T>(Func<IDbConnection, IDbTransaction, Task<T>> func)
+        {
+            return await this.ExecuteAsync(async connection =>
+            {
+                var transaction = connection.BeginTransaction();
+                try
+                {
+                    var result = await func(connection, transaction);
                     transaction.Commit();
                     return result;
                 }
--- LuckyStarry/LuckyStarry.Data/IDbClient.cs
+++ LuckyStarry/LuckyStarry.Data/IDbClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LuckyStarry.Data
 {
@@ -17,5 +18,15 @@ namespace LuckyStarry.Data
         T QueryFirstOrDefault<T>(string sqlText, object param = null);
 
         T Transaction<T>(Func<IDbConnection, IDbTransaction, T> func);
+
+        Task<IDbConnection> CreateConnectionAsync();
+        Task<T> ExecuteAsync<T>(Func<IDbConnection, Task<T>> func);
+        Task<int> ExecuteNonQueryAsync(string sqlText, object param = null);
+        Task<T> ExecuteScalarAsync<T>(string sqlText, object param = null);
+        Task<IEnumerable<T>> QueryAsync<T>(string sqlText, object param = null);
+        Task<T> QueryFirstAsync<T>(string sqlText, object param = null);
+        Task<T> QueryFirstOrDefaultAsync<T>(string sqlText, object param = null);
+
+        Task<T> TransactionAsync<T>(Func<IDbConnection, IDbTransaction, Task<T>> func);
     }
 }
