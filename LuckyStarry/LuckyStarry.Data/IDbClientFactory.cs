ffa13de2c2b47afa46ba975537303f4906aae503
SUN BO <starry@vip.qq.com>
2017-12-19 16:38:24 +0800

    DbClient Async Functions +

--- LuckyStarry/LuckyStarry.Data/IDbClientFactory.cs
+++ LuckyStarry/LuckyStarry.Data/IDbClientFactory.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LuckyStarry.Data
 {
     public interface IDbClientFactory
     {
         IDbClient Create(string dbName);
+        Task<IDbClient> CreateAsync(string dbName);
     }
 }
--- LuckyStarry/LuckyStarry.Data/IDbConnectionFactory.cs
+++ LuckyStarry/LuckyStarry.Data/IDbConnectionFactory.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LuckyStarry.Data
 {
     public interface IDbConnectionFactory
     {
         IDbConnection Create(string name);
+        Task<IDbConnection> CreateAsync(string name);
     }
 }
