d8fe076d727420dc92397839053b289e70d23e55
SUN BO <starry@vip.qq.com>
2017-12-20 21:44:39 +0800

    ISqlConditionFactory

--- LuckyStarry/LuckyStarry.Data/ICommandFactory.cs
+++ LuckyStarry/LuckyStarry.Data/ICommandFactory.cs
@@ -10,5 +10,7 @@ namespace LuckyStarry.Data
         IInsertBuilder CreateInsertBuilder();
         IUpdateBuilder CreateUpdateBuilder();
         IDeleteBuilder CreateDeleteBuilder();
+
+        ISqlConditionFactory GetConditionFactory();
     }
 }
---
+++ LuckyStarry/LuckyStarry.Data/ISqlConditionFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuckyStarry.Data
+{
+    public interface ISqlConditionFactory
+    {
+        ISqlCondition EqualTo(string column, string parameter);
+        ISqlCondition NotEqualTo(string column, string parameter);
+        ISqlCondition LessThan(string column, string parameter);
+        ISqlCondition LessThanOrEqualTo(string column, string parameter);
+        ISqlCondition GreaterThan(string column, string parameter);
+        ISqlCondition GreaterThanOrEqualTo(string column, string parameter);
+    }
+}
