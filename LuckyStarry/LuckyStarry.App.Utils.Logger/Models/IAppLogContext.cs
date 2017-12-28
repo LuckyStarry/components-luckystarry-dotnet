7eb4043089c20f08353aef8f601fa1ac5f93178b
SUN BO <starry@vip.qq.com>
2017-12-28 10:49:24 +0800

    IAppLogger

---
+++ LuckyStarry/LuckyStarry.App.Utils.Logger/Models/IAppLogContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuckyStarry.App.Utils.Logger.Models
+{
+    public interface IAppLogContext
+    {
+        string Message { set; get; }
+        string Module { set; get; }
+        string Catelog { set; get; }
+        LogLevel Level { set; get; }
+
+        string this[string key] { set; get; }
+
+        void Save();
+    }
+}
