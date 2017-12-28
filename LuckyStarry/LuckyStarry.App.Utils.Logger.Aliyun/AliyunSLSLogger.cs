7ec023deb6d918b5aa71e09074439a9c5510e97a
SUN BO <starry@vip.qq.com>
2017-12-28 11:17:00 +0800

    LuckyStarry.App.Utils.Logger.Aliyun.1.0.0

---
+++ LuckyStarry/LuckyStarry.App.Utils.Logger.Aliyun/AliyunSLSLogger.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LuckyStarry.App.Utils.Logger.Models;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using Aliyun.Api.LOG;
+using Aliyun.Api.LOG.Data;
+using System.Threading;
+using Aliyun.Api.LOG.Request;
+using System.Linq;
+
+namespace LuckyStarry.App.Utils.Logger
+{
+    public class AliyunSLSLogger : IAppLogger, IDisposable
+    {
+        private readonly LogClient client;
+        private readonly IAppInfo appInfo;
+        private readonly string project;
+        private readonly string logstore;
+        private readonly object syncLock = new object();
+        private BlockingCollection<LogItem> logContexts;
+
+        public AliyunSLSLogger(IAppInfo appInfo, string project, string logstore, string endpoint, string accessKeyId, string accessKey)
+        {
+            this.RecreateList();
+            this.appInfo = appInfo;
+            this.project = project;
+            this.logstore = logstore;
+            this.client = new LogClient(endpoint, accessKeyId, accessKey);
+            this.consumer = new Timer(state => this.Consume(), null, 0, 5 * 1000);
+        }
+
+        IAppLogContext IAppLogger.CreateLogContext() => this.CreateLogContext();
+        public AliyunSLSLogContext CreateLogContext() => new AliyunSLSLogContext(this);
+
+        public void Save(AliyunSLSLogContext logContext)
+        {
+            var logItem = logContext.ToLogItem();
+            if (logItem != null)
+            {
+                logItem.PushBack("Environment", this.appInfo.GetEnviroment());
+                this.logContexts.TryAdd(logItem);
+            }
+        }
+
+        private BlockingCollection<LogItem> RecreateList()
+        {
+            lock (this.syncLock)
+            {
+                var logContexts = this.logContexts;
+                this.logContexts = new BlockingCollection<LogItem>(100);
+                return logContexts;
+            }
+        }
+
+        private Timer consumer;
+        private void Consume()
+        {
+            try
+            {
+                var logContexts = this.RecreateList();
+                if (logContexts != null && logContexts.Count > 0)
+                {
+                    var putLogsRequest = new PutLogsRequest();
+                    putLogsRequest.Project = this.project;
+                    putLogsRequest.Topic = this.appInfo.GetAppName();
+                    putLogsRequest.Logstore = this.logstore;
+                    putLogsRequest.LogItems = logContexts.ToList();
+                    this.client.PutLogs(putLogsRequest);
+                }
+            }
+            finally { }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // 要检测冗余调用
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: 释放托管状态(托管对象)。
+                }
+
+                // TODO: 释放未托管的资源(未托管的对象)并在以下内容中替代终结器。
+                // TODO: 将大型字段设置为 null。
+                this.consumer.Dispose();
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: 仅当以上 Dispose(bool disposing) 拥有用于释放未托管资源的代码时才替代终结器。
+        // ~AliyunSLSLogger() {
+        //   // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
+        //   Dispose(false);
+        // }
+
+        // 添加此代码以正确实现可处置模式。
+        void IDisposable.Dispose()
+        {
+            // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
+            Dispose(true);
+            // TODO: 如果在以上内容中替代了终结器，则取消注释以下行。
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
---
+++ LuckyStarry/LuckyStarry.App.Utils.Logger.Aliyun/Models/AliyunSLSLogContext.cs
@@ -0,0 +1,66 @@
+﻿using Aliyun.Api.LOG.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LuckyStarry.App.Utils.Logger.Models
+{
+    public sealed class AliyunSLSLogContext : IAppLogContext
+    {
+        internal AliyunSLSLogContext(AliyunSLSLogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public string Message { set; get; }
+        public string Module { set; get; }
+        public string Catelog { set; get; }
+        public LogLevel Level { set; get; } = LogLevel.Info;
+
+        private IDictionary<string, string> items = new Dictionary<string, string>();
+        public string this[string key]
+        {
+            set => this.items[key] = value;
+            get => this.items[key];
+        }
+
+        public void Save()
+        {
+            this.logger.Save(this);
+        }
+
+        public LogItem ToLogItem()
+        {
+            if (string.IsNullOrWhiteSpace(this.Message))
+            {
+                return null;
+            }
+            var logItem = new LogItem { };
+            logItem.Time = TimeSpan();
+            logItem.PushBack(nameof(this.Message), this.Message);
+            logItem.PushBack(nameof(this.Level), this.Level.ToString());
+            if (!string.IsNullOrWhiteSpace(this.Module))
+            {
+                logItem.PushBack(nameof(this.Module), this.Module);
+            }
+            if (!string.IsNullOrWhiteSpace(this.Catelog))
+            {
+                logItem.PushBack(nameof(this.Catelog), this.Catelog);
+            }
+            if (this.items != null && this.items.Any())
+            {
+                foreach (var kv in this.items.OrderBy(kv => kv.Key))
+                {
+                    logItem.PushBack($"custom.{ kv.Key }", kv.Value ?? string.Empty);
+                }
+            }
+            return logItem;
+        }
+
+        private static DateTime _1970StartDateTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1, 0, 0, 0, 0));
+        private readonly AliyunSLSLogger logger;
+
+        public static uint TimeSpan() => (uint)Math.Round((DateTime.Now - _1970StartDateTime).TotalSeconds, MidpointRounding.AwayFromZero);
+    }
+}
