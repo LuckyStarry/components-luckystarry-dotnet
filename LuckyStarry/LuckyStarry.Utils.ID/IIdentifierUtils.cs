43f29915dab916562007c59dad1e8f3366bad8d8
SUN BO <starry@vip.qq.com>
2018-01-08 15:20:10 +0800

    LuckyStarry.Utils.ID.1.0.0

---
+++ LuckyStarry/LuckyStarry.Utils.ID/IIdentifierUtils.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuckyStarry.Utils.ID
+{
+    public interface IIdentifierUtils
+    {
+    }
+}
---
+++ LuckyStarry/LuckyStarry.Utils.ID/Identifier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuckyStarry.Utils.ID
+{
+    public interface Identifier
+    {
+        string Version { get; }
+
+        long Value { get; }
+    }
+}
